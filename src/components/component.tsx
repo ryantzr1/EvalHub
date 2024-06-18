788be2f572f947373a3f9d78c01978e92bb5938a
ryantzr1 <dreisprung1821@gmail.com>
2024-06-18 14:31:10 +0800

    EvalHub simplify evaluation platform v0

--- src/components/component.tsx
+++ src/components/component.tsx
@@ -6,17 +6,8 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { taskCategories } from "@/data/taskCategories"; // Adjust the import path as needed
 
-const taskCategories = {
-  "Language Understanding": ["aclue", "anli", "blimp", "ceval", "glue", "siqa", "super_glue"],
-  "Mathematical and Logical Reasoning": ["arithmetic", "asdiv", "gsm8k", "hendrycks_math", "logiqa", "mathqa", "minerva_math"],
-  "Question Answering": ["aexams", "arc", "babi", "coqa", "csatqa", "drop", "headqa", "lambada", "medmcqa", "medqa", "nq_open", "openbookqa", "pubmedqa", "qa4mre", "qasper", "race", "sciq", "squad_completion", "squadv2", "triviaqa", "truthfulqa"],
-  "Commonsense Reasoning": ["agieval", "commonsense_qa", "hellaswag", "piqa", "winogrande"],
-  "Cross-lingual and Multilingual Tasks": ["basqueglue", "belebele", "cmmlu", "eus_exams", "eus_proficiency", "eus_reading", "eus_trivia", "kobest", "kormedmcqa", "lambada_multilingual", "lambada_multilingual_stablelm", "mgsm", "okapi/arc_multilingual", "okapi/hellaswag_multilingual", "okapi/mmlu_multilingual", "okapi/truthfulqa_multilingual", "paws-x", "translation", "xcopa", "xnli", "xnli_eu", "xstorycloze", "xwinograd"],
-  "Ethics and Bias": ["crows_pairs", "eq_bench", "hendrycks_ethics", "realtoxicityprompts", "toxigen"],
-  "Specialized Domains": ["code_x_glue", "copal_id", "fda", "headqa", "ifeval", "logiqa2", "prost", "sciq", "tmmluplus", "toxigen"],
-  "Miscellaneous": ["bigbench", "benchmarks", "bbh", "gpt_3", "mmlu", "model_written_evals", "pile", "pile_10k", "scrolls", "swde", "tinyBenchmarks", "unitxt"]
-};
 
 export default function Component() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -70,7 +61,7 @@ export default function Component() {
       const matchesCategory = category === "all" || taskCategories[category].map(task => task.toLowerCase()).includes(metric.name.toLowerCase());
       return matchesSearch && matchesCategory;
     });
-  }, [searchTerm, metrics, category]);
+  }, [metrics, searchTerm, category]);
 
   const handleMetricSelect = (metric: any) => {
     const updatedMetrics = selectedMetrics.includes(metric)
