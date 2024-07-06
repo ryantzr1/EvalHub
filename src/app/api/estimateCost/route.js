c1ea2d4c6823b529713fc3a896166984e877c371
ryantzr1 <dreisprung1821@gmail.com>
2024-07-06 22:16:50 +0800

    Fix cors issue

--- src/app/api/estimateCost/route.js
+++ src/app/api/estimateCost/route.js
@@ -1,80 +1,10 @@
 import { NextResponse } from "next/server";
-import tiktoken from "tiktoken";
 
 export const revalidate = 0;
 
 export async function POST(request) {
   try {
-    const formData = await request.formData();
-    const file = formData.get("file");
-    const huggingfaceDataset = formData.get("huggingface_dataset");
-    const model = formData.get("model") || "gpt-4";
-
-    let dataset;
-    if (file) {
-      const fileContent = await file.text();
-      dataset = fileContent.split("\n");
-    } else if (huggingfaceDataset) {
-      try {
-        dataset = JSON.parse(huggingfaceDataset);
-      } catch (error) {
-        return NextResponse.json(
-          { error: "Error parsing Hugging Face dataset: " + error.message },
-          { status: 400 }
-        );
-      }
-    } else {
-      return NextResponse.json(
-        { error: "No file or dataset provided" },
-        { status: 400 }
-      );
-    }
-
-    // Fetch model costs
-    let modelCosts;
-    try {
-      const MODEL_COSTS_URL =
-        "https://raw.githubusercontent.com/ryantzr1/EvalHub/main/src/lib/model_prices.json";
-      const response = await fetch(MODEL_COSTS_URL);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      modelCosts = await response.json();
-    } catch (error) {
-      return NextResponse.json(
-        { error: "Error fetching model costs: " + error.message },
-        { status: 500 }
-      );
-    }
-
-    if (!modelCosts[model]) {
-      return NextResponse.json(
-        { error: "Invalid model specified" },
-        { status: 400 }
-      );
-    }
-
-    const encoder = tiktoken.encoding_for_model(model);
-
-    let totalTokens = 0;
-    for (const text of dataset) {
-      totalTokens += encoder.encode(text).length;
-    }
-
-    const totalCost = totalTokens * modelCosts[model].input_cost_per_token;
-
-    const response = NextResponse.json({
-      model,
-      total_token_count: totalTokens,
-      total_cost: totalCost,
-    });
-    response.headers.set("Access-Control-Allow-Origin", "*");
-    response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    response.headers.set(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    return response;
+    return NextResponse.json({ message: "Hello, world!" });
   } catch (error) {
     console.error("Error in POST handler:", error);
     const errorResponse = NextResponse.json(
